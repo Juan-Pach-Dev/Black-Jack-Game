e1f60e3eeab79d3ab0109bc32e928c1bbc894531
Juan-Pach-Dev <jusepamo01@hotmail.com>
2021-10-04 17:52:34 -0500

    Formularios Creados J Perez y J Pasichana

--- Player.cs
+++ Player.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Black_Jack_Game
 {
-    public partial class Form1 : Form
+    public partial class Player : Form
     {
-        public Form1()
+        public Player()
         {
             InitializeComponent();
         }
