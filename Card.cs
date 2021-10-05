0d198ea5fb4a732373e1e280f22de95fafefd5a3
Juan-Pach-Dev <jusepamo01@hotmail.com>
2021-10-04 21:23:16 -0500

    Dealer Hecho J Perez - J Pasichana

--- Card.cs
+++ Card.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Black_Jack_Game
 {
-    public partial class Card : Form
+    public class Card
     {
-        public Card()
-        {
-            InitializeComponent();
-        }
+        private char forma;
+        private string simbolo;
+        private int valor;
+        private string color;
+
+        public char Forma { get => forma; set => forma = value; }
+        public string Simbolo { get => simbolo; set => simbolo = value; }
+        public int Valor { get => valor; set => valor = value; }
+        public string Color { get => color; set => color = value; }
+
     }
-}
+
+
+    }
+
--- Dealer.cs
+++ Dealer.cs
@@ -1,20 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Black_Jack_Game
 {
-    public partial class Dealer : Form
+    class Dealer
     {
+        List<Card> Deck;
+        int HandDeck;
+
         public Dealer()
         {
-            InitializeComponent();
+            HandDeck = 53;
+            Deck = new List<Card>(HandDeck);
         }
+
+        public Dealer(int hand)
+        {
+            HandDeck = hand;
+            Deck = new List<Card>(HandDeck);
+        }
+
+        public List<Card> getDealer()
+        {
+            return Deck;
+        }
+
+        public void generate()
+        {
+            Card newCard;
+            for (int p = 0; p <= 52; p++)
+            {
+                if (p < 13)
+                {
+                    if (p == 0)
+                    {
+                        newCard = new Card(1, 14, "rojo");
+                    }
+                    else
+                    {
+                        newCard = new Card(1, p + 1, "rojo");
+
+                    }
+                }
+                else if (p < 26)
+                {
+                    if (p == 13)
+                    {
+                        newCard = new Card(2, 14, "rojo");
+                    }
+                    else
+                    {
+                        newCard = new Card(2, p - 12, "rojo");
+                    }
+                }
+                else if (p < 39)
+                {
+                    if (p == 26)
+                    {
+                        newCard = new Card(3, 14, "rojo");
+                    }
+                    else
+                    {
+                        newCard = new Card(3, p - 25, "rojo");
+                    }
+
+                }
+                else
+                {
+                    if (p == 39)
+                    {
+                        newCard = new Card(4, 14, "rojo");
+                    }
+                    else
+                    {
+                        newCard = new Card(4, p - 39, "rojo");
+
+                    }
+                }
+                Deck.Add(newCard);
+            }
+
+        }
+        public void emptyDeck(int hand)
+        {
+            Deck = new List<Card>(hand);
+        }
+        public void showDeck()
+        {
+            for (int p = 0; p < HandDeck; p++)
+            {
+                Console.WriteLine("Suit: " + Deck[p].getSuitString() + "Score" + Deck[p].getScoreString() + "Color" + Deck[p].getColorString();
+            }
+        }
+
+        public void generate(int maxScore, int minScore, int numOfsuits, String nameOfSuits)
+        {
+
+        }
+
+
+        public void randomize()
+        {
+            Random myRandom = new Random();
+            List<Card> tDeck = new List<Card>(HandDeck);
+
+            int randomNumber = 0;
+            for (int p = 0; p < 53; p++)
+            {
+                randomNumber = myRandom.Next(0, HandDeck);
+                tDeck.Add(Deck[randomNumber]);
+                Deck.RemoveAt(randomNumber);
+                HandDeck--;
+            }
+            for (int p = 0; p < 53; p++)
+            {
+                Deck.Add(tDeck[p]);
+            }
+        }
+
+
     }
 }
